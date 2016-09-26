fb147876fd7a5433203e73a8abac6f0722abd7cd
phusitkrittikarak <sutamoo@hotmail.com>
2016-09-27 01:41:37 +0700

    580610669 Finish HW4

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -27,7 +27,7 @@ public class Student {
     public boolean addCourse(Course c) {
         if (c.enrollStudent(this)) {    // enroll the course with "this" student object
             // add the new course to the list of enrolled courses (PList)
-            // implement your code here!!!
+            courses.pushToTail(c);
 
             return true;
         } else
@@ -36,9 +36,12 @@ public class Student {
 
     public boolean dropCourse(Course c) {
         // remove "this" student from the course
-        // implement your code here!!!
-
-        return false;
+        if (c.removeStudent(this))
+        {
+            courses.remove(c);
+            return true;
+        }
+        else return false;
     }
 
     public String getName() {
