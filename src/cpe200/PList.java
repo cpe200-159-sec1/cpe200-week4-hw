14537fcee33c0524127e9e2a44bcc4b070173d4f
KaninSukarote <kanin.sukarote@gmail.com>
2016-09-28 17:35:58 +0700

    580610622

--- src/cpe200/PList.java
+++ src/cpe200/PList.java
@@ -62,10 +62,21 @@ public class PList {
 
         while (tmp != null) {
             if (tmp.data.equals(data)) {
-                // implement your code here!!!
-                // case 1: head of the list
-                // case 2: tail of the list
-                // case 3: somewhere in the middle
+                if (tmp.data.equals(data)) {
+                    if (tail == head) {
+                        head = tail = null;
+                        } else if (tmp == tail) {
+                        tail = tmp.prev;
+                        tail.next = null;
+                        } else if (tmp == head) {
+                        head = head.next;
+                        head.prev = null;
+                        } else {
+                        tmp.prev.next = tmp.next;
+                        tmp.next.prev = tmp.prev;
+                        }
+                    return true;
+                }
             }
             tmp = tmp.next;
         }
@@ -73,15 +84,28 @@ public class PList {
     }
 
     public Object elementAt(int index) {
-        // implement your code here!!!
-        // what if index is not in between 0 to (size-1)
-
-        return null;
+        if(index < 0 || index >= size){
+            return null;
+            }else{
+            PNode tmp = head;
+            for(int i = 0 ; i < index ; i++) {
+                        tmp = tmp.next;
+            }
+            return tmp.data;
+        }
     }
 
     // rename the search method to "found(Object data)"
     public boolean found(Object data) {
-        // implement your code here!!!
+        if(!isEmpty()){
+            PNode temp = head;
+            while (temp != null){
+                if(temp.data.equals(data)){
+                    return true;
+                }
+                temp = temp.next;
+                }
+            }
 
         return false;
     }
